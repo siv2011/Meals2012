d887b446d8e371895cd90ee2de4b1b98987f6a9a
Sivakami Sambasivam <ssivakami@alum.mit.edu>
2012-06-22 01:55:24 -0400

    Making it pretty step 2.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,12 @@ class StaticPagesController < ApplicationController
   def home
   end
 
+  def about
+  end
+
   def help
+  end
+
+  def contact
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Meals2012::Application.routes.draw do
   resources :meals
-  get "static_pages/home"
-  get "static_pages/help"
+  root to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+ 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
